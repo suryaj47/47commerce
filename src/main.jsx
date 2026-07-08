ac80689396c4f4ca24a4e03df56146411f1093b7
suryajp47 <suryaj4747@gmail.com>
2026-07-08 19:14:03 +0530

    fix: Correct import path for App component in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { CartProvider } from "./context/CartContext";
 import ReactDOM from "react-dom/client";
 import { ClerkProvider } from "@clerk/clerk-react";
 import "./styles/global.css";
-import App from "./App";
+import App from "./app";
 const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
